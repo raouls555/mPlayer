3f6e9068cf47a9c21ef9f84a7d7dbe5061c00177
raouls555 <r.smitsmail@gmail.com>
2020-02-19 09:18:22 +0100

    converted full version over to modules

--- js/main.js
+++ js/main.js
@@ -1,10 +1,19 @@
-const c = document.getElementById("container");
-const playButton = document.getElementById("b");
-const dur = document.getElementById("dur");
+import {Mplayer} from './Mplayer.js';
+import {addButtonInput} from './inputs.js';
+import { CanvasMusicVis } from './canvasMusicVis.js';
+
 const currentSong = document.getElementById("currentSong");
 
-const showBTN = document.getElementById("showPlaylistBtn");
 const playlist = document.getElementById("playlist");
+
+const playButton = addButtonInput('play',playClick);
+const showBTN = addButtonInput('showPlaylist',()=>showPlaylist());
+addButtonInput('upload',uploadClick);
+addButtonInput('shuffle',()=>music.shuffle());
+addButtonInput('remove',()=>music.remove());
+addButtonInput('back',()=>music.currentFile--);
+addButtonInput('forw',()=>music.currentFile++);
+addButtonInput('hidePlaylist',()=>hidePlaylist());
 
 const music = new Mplayer(document.getElementById("sliderCon"),document.getElementById("gui"));
 
@@ -19,23 +28,36 @@ window.addEventListener('keydown',function(e){
     else if(e.key === 'ArrowDown') music.currentFile = music.currentFile;
     else if(e.key === 's') music.shuffle();
     else if(e.key === 'h') document.body.classList.toggle('hide');
+    else if(e.key === 'd') vis.activeStyle++;
+    else if(e.key === 'a') vis.activeStyle--;
+    else if(e.key === 'c') vis.activeColor++;
+    else if(e.key === 'z') vis.activeColor--;
+    else if(e.key === 'h'){
+        vis.offset = document.body.classList.contains('hide') ? 0 : 1.8;
+        vis.updateSize();
+    } 
+    else if(parseInt(e.key) > 0) vis.gain.gain.value = parseInt(e.key).map(0,9,0,1.8);
     else return false;
-
     return false;
 });
 
-function playClick(e){
-    if(e.innerText == "play"){
+function playClick(){
+    if(playButton.innerText == "play"){
         music.m.play();
-        e.innerText = "stop";
+        playButton.innerText = "stop";
     } else{
         music.m.pause();
-        e.innerText = "play";
+        playButton.innerText = "play";
     }
 }
-
+let vis;
 function uploadClick(){
     music.fileSelector.click();
+    if(vis) return;
+    let context = new AudioContext();
+    let sauce = context.createMediaElementSource(music.m);
+    vis = new CanvasMusicVis(context,sauce,document.querySelector('canvas'),13);
+    vis.draw();
 }
 
 function showPlaylist(){
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   mode: 'development',
   entry: {
-    main: './js/main.js',
+    full: './js/main.js',
     lite: './js/lite.js'
   },
   output: {
